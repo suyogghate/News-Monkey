81f9919d500dce75aaf7b9164888317343125bda
Ghate Suyog <suyogghateminks@gmail.com>
2022-11-02 17:44:09 +0530

    made navbar, newsitem, news component, used bootstrap for cards, fetch api from news api

--- src/components/News.js
+++ src/components/News.js
@@ -4,9 +4,22 @@ import NewsItem from './NewsItem';
 export class News extends Component {
   render() {
     return (
-      <div>
-        This is a news component
-        <NewsItem />
+      <div className='container my-3'>
+        <h2>News Monkey - Top Headlines</h2>
+        <div className="row">
+            <div className="col-md-4">
+                <NewsItem title = "myTitle" description="myDescrption"/>
+            </div>
+            <div className="col-md-4">
+                <NewsItem title = "myTitle" description="myDescrption"/>
+            </div>
+            <div className="col-md-4">
+                <NewsItem title = "myTitle" description="myDescrption"/>
+            </div>
+            <div className="col-md-4">
+                <NewsItem title = "myTitle" description="myDescrption"/>
+            </div>
+        </div>
       </div>
     )
   }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
+    let {title, description} = this.props;
     return (
       <div>
-        This is a news item.
+        <div className="card" style={{width: "18rem"}}>
+            <img src="https://cdn.24.co.za/files/Cms/General/d/7338/2a2392d38baf4049adc03d59694c89c4.jpg" className="card-img-top" alt="..."/>
+                <div className="card-body">
+                    <h5 className="card-title">{title}</h5>
+                    <p className="card-text">{description}</p>
+                    <a href="/newsdetail" className="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
       </div>
     )
   }
