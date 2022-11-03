4b4ce929066ccc6299160a614b2b75ebe38e69c6
Ghate Suyog <suyogghateminks@gmail.com>
2022-11-03 19:28:30 +0530

    using fetch api to populate items

--- src/components/News.js
+++ src/components/News.js
@@ -2,65 +2,32 @@ import React, { Component } from 'react';
 import NewsItem from './NewsItem';
 
 export class News extends Component {
-    articles = [
-        {
-            "source": { "id": "news24", "name": "News24" },
-            "author": "Compiled by Lynn Butler",
-            "title": "Proteas plot life after Boucher as Maketa named interim coach for Australia tour",
-            "description": "Cricket South Africa has named Malibongwe Maketa as the Proteas interim head coach for the upcoming three-match Test series against Australia.",
-            "url": "https://www.news24.com/sport/cricket/proteas/bouchers-replacement-confirmed-as-maketa-named-interim-proteas-coach-for-australia-tour-20221102",
-            "urlToImage": "https://cdn.24.co.za/files/Cms/General/d/7338/2a2392d38baf4049adc03d59694c89c4.jpg",
-            "publishedAt": "2022-11-02T10:30:52+00:00",
-            "content": "<ul><li>Cricket SA has named Malibongwe Maketa as the Proteas' interim head coach for the upcoming Test series in Australia.</li><li>Maketa will replace Mark Boucher, who will step down from his role… [+2605 chars]"
-          },
-          {
-            "source": { "id": "news24", "name": "News24" },
-            "author": "AFP",
-            "title": "Wallabies-All Blacks combined team could play Lions - reports",
-            "description": "The Wallabies and All Blacks are in talks to form a joint team to face the British and Irish Lions in a one-off Test at the Melbourne Cricket Ground in 2025, reports said Wednesday.",
-            "url": "https://www.news24.com/sport/rugby/wallabies-all-blacks-combined-team-could-play-lions-reports-20221102",
-            "urlToImage": "https://cdn.24.co.za/files/Cms/General/d/7810/08306c44dcf24332898a981ffaa6deaa.jpg",
-            "publishedAt": "2022-11-02T07:07:53+00:00",
-            "content": "The Wallabies and All Blacks are in talks to form a joint team to face the British &amp; Irish Lions in a one-off Test at the Melbourne Cricket Ground in 2025, reports said on Wednesday.\r\nThe Lions, … [+1294 chars]"
-          },
-          {
-            "source": { "id": "espn-cric-info", "name": "ESPN Cric Info" },
-            "author": null,
-            "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-            "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-            "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-            "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-            "publishedAt": "2020-04-27T11:41:47Z",
-            "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
-          },
-          {
-            "source": { "id": "espn-cric-info", "name": "ESPN Cric Info" },
-            "author": null,
-            "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-            "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-            "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-            "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-            "publishedAt": "2020-03-30T15:26:05Z",
-            "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
-          }
-    ]
+
     constructor(){
         super();
         console.log("Hello i am constructor from news component.");
         this.state = {
-            articles: this.articles,
-            loading: false
+            articles: [],
+            loading: true
         }
+    }
+
+    async componentDidMount(){
+        let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=b9f68b2501734c11b42dfed59f9baf80";
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        console.log(parsedData);
+        this.setState({articles: parsedData.articles})
     }
 
   render() {
     return (
       <div className='container my-3'>
-        <h2>News Monkey - Top Headlines</h2>
+        <h1>News Monkey - Top Headlines</h1>
         <div className="row">
         {this.state.articles.map((element)=> {
             return <div className="col-md-4" key={element.url}>
-            <NewsItem title = {element.title.slice(0, 40)} description={element.description.slice(0, 80)} imageUrl={element.urlToImage} newsUrl={element.url}/>
+            <NewsItem title = {element.title?element.title:""} description={element.description?element.description:""} imageUrl={element.urlToImage} newsUrl={element.url}/>
             </div>
         })}
         </div>
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -8,7 +8,7 @@ export class NewsItem extends Component {
     return (
       <div className='my-3'>
         <div className="card" style={{width: "18rem"}}>
-            <img src={imageUrl} className="card-img-top" alt="..."/>
+            <img src={!imageUrl?"https://images.moneycontrol.com/static-mcnews/2022/02/HDFC-Ltd6-770x433.jpg":imageUrl} className="card-img-top" alt="..."/>
                 <div className="card-body">
                     <h5 className="card-title">{title}....</h5>
                     <p className="card-text">{description}....</p>
